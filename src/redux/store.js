bc6096adbe57e31db046889dbffae9c0d957c0cf
annafrymark <anna.frymark@gmail.com>
2023-05-04 20:33:12 +0200

    reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,6 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
-import { statusFilters } from './constants';
-
-const initialState = {
-  tasks: [
-    { id: 0, taxt: 'Learn HTML and CSS', completed: true },
-    { id: 1, taxt: 'Get good at JavaScript', completed: true },
-    { id: 2, taxt: 'Master React', completed: false },
-    { id: 3, taxt: 'Discover Redux', completed: false },
-    { id: 4, taxt: 'Build amazing apps', completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
-  },
-};
-
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
+import { rootReducer } from './reducer';
 
 const enhancer = devToolsEnhancer();
 
